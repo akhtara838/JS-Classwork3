fc1ba4e8ec830d573d3c23003c6af5fd400f3e83
Ayesha Akhtar <ayeshaakhtar13@gmail.com>
2021-03-08 04:13:00 -0700

    installed es6 string html

--- js/components/bookDisplay.js
+++ js/components/bookDisplay.js
@@ -7,8 +7,9 @@ app.component('book-display', {
         }
     },
     template:
-    `<ul class="col-sm-12 col-md-6 col-xl-4>
-    <a class = "list-group-item list-group-item-action active" :href ="this.bookObj.selfLink">{{this.bookObj.volumeInfo.title}}</a>
+    /*html*/
+    `<ul class="col-sm-12 col-md-6 col-xl-4">
+    <a class = "list-group-item list-group-item-action active" :href ="this.bookObj.selfLink ">{{this.bookObj.volumeInfo.title}}</a>
     <li class="list-group-item">{{this.bookObj.volumeInfo.authors}}</li>
     <li class = "list-group-item">{{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
    </ul>`,
